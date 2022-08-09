a33051d3032ce9a90f48c81827394dcebeadec76
r-bjorn <ravidtrivedi111@gmail.com>
2022-08-09 14:24:05 -0400

    Fixing bugs

--- Level_3_1/solution.py
+++ Level_3_1/solution.py
@@ -1,20 +1,9 @@
-def getXOR(start, end):
-
-    if (end - start) == 0:
-        return 0
-    if (end - start) == 1:
-        return start
-    if (end - start) <= 4:
-        return reduce(lambda x, y: x ^ y, range(start, end))
-    else:
-        begin_range = (start, start / 4 * 4 + 4)
-        end_range = (end / 4 * 4, end)
-        return getXOR(*begin_range) ^ getXOR(*end_range)
-
-
-def solution(start, length):
-    worker_list = [(start + (length - l) * length, start + (length - l) * length + l) for l in range(length, 0, -1)]
-    
-    new_xor = [getXOR(start, end) for start, end in worker_list]
-    
-    return reduce(lambda x, y: x ^ y, new_xor)
+def solution(x, y):
+    M, F = max(int(x), int(y)), min(int(x), int(y))
+    res = 0
+    while F > 0:
+        res += M // F
+        M, F = F, M % F
+    if M != 1:
+        return "impossible"
+    return str(res - 1)
